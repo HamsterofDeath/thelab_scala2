41f1dbfb5a90478087740faffecb859485a5daf8
HamsterofDeath <h-star@gmx.de>
2018-10-06 18:34:25 +0200

    .

--- src/hod/euler/Euler100.scala
+++ src/hod/euler/Euler100.scala
@@ -2,7 +2,13 @@ package hod.euler
 
 object Euler100 {
   def main(args: Array[String]): Unit = {
-    def t(b:Long) ={
+    def evalBOfTotal(t:Long) = {
+      // thx to wolfram alpha
+      val tt2 = BigInt(2L) * t * t
+      val t2p1 = BigInt(2L) * t
+      ((tt2 - t2p1 + 1).sqrt(25)+1) * 0.5d
+    }
+    def evalTotalOf(b:Long) ={
       val magic = {
         BigInt(8L) * b * b - BigInt(8) * b + 1
       }
@@ -10,12 +16,13 @@ object Euler100 {
       0.5d * (magic2 + 1)
     }
 
-    val hard = 100000000000L
-    val easy = 1L
-    Iterator.iterate(easy)(_ + 1).foreach { b =>
-      val total = t(b)
-      if (total.isValidLong) {
-        println(s"$b blue, ${total - b} red")
+    val hard = 10000000000000L
+    val easy = 21L
+    Iterator.iterate(hard)(_ + 1).foreach { t =>
+      val blues = evalBOfTotal(t)
+      if (blues.isValidLong) {
+        val red = t - blues
+        println(s"$blues blue, ${red} red = ${blues+red} total, result = ${(blues/t) * ((blues-1)/(t-1))}")
       }
     }
   }
