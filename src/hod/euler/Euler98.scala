6ef76ea4f3961d76dd5c820dd502f05dcbbc9f31
hamsterofdeath <h-star@gmx.de>
2018-10-15 14:22:42 +0200

    .

--- src/hod/euler/Euler98.scala
+++ src/hod/euler/Euler98.scala
@@ -5,11 +5,55 @@ import scala.reflect.io.File
 object Euler98 {
   def main(args: Array[String]): Unit = {
     val file = File("resource/p98words.txt")
-    val words = file.slurp().split(',').map(_.drop(1).dropRight(1))
+    val words = {
+      file
+      .slurp()
+      .split(',')
+      .map(_.drop(1).dropRight(1))
+    }
     val anagrams = words.groupBy(_.sorted).values.toList.filter(_.length > 1)
     val maxChars = anagrams.map(_.head.length).max
     val maxValue = math.pow(10, maxChars + 1).toLong
-    val squareNumbers = allSquares.takeWhile(_ <= maxValue).map(_.toString).toList.groupBy(_.sorted).values.toList.filter(_.length > 1)
-    println()
+    val squareNumbers = {
+      allSquares
+      .takeWhile(_ <= maxValue)
+      .map(_.toString)
+      .toList
+      .groupBy(_.sorted)
+      .values
+      .toList
+      .filter(_.length > 1)
+      .flatten
+      .groupBy(_.length)
+
+    }
+    val solutions = {
+      anagrams.flatMap { words =>
+        def mappingsOf(word: String) = {
+          val fittingSquares = squareNumbers.getOrElse(word.length, Nil)
+          val possibleMappings = {
+            fittingSquares.map { digits =>
+              digits.zip(word)
+            }.filter { digitToChar =>
+              digitToChar.toMap == digitToChar.reverse.toMap
+            }.map(_.toMap)
+          }
+          possibleMappings
+        }
+        val mappings = words.map(mappingsOf)
+        val intersection = mappings.foldLeft(mappings.head)((acc,e) => acc.intersect(e))
+        val good = intersection.nonEmpty
+        if (good) {
+          Some {
+            words.toList -> intersection
+          }
+        } else {
+          None
+        }
+      }
+    }
+    solutions.map { case (words, mappings) =>
+
+    }
   }
 }
