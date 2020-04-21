8e975a5048f799ab7c6872a237612c1124a46f01
Dennis Haupt <d.haupt@veact.net>
2020-04-21 16:02:36 +0200

    euler 3

---
+++ src/main/scala/hod/euler/Euler3.scala
@@ -0,0 +1,17 @@
+package hod.euler
+
+object Euler3 {
+  def main(args: Array[String]): Unit = {
+    val num      = 600851475143L
+    val limit    = num.sqrt.toLong
+    val solution = {
+      allPrimesLong
+        .takeWhile(_ <= limit)
+        .filter { prime =>
+          num % prime == 0
+        }
+        .max
+    }
+    println(solution)
+  }
+}
