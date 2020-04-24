27ff4787fd59b84e5bc3b662c1db48496c13bf39
Dennis Haupt <d.haupt@veact.net>
2020-04-25 01:57:18 +0200

    euler 89

--- src/main/scala/hod/euler/Euler89.scala
+++ src/main/scala/hod/euler/Euler89.scala
@@ -3,6 +3,77 @@ package hod.euler
 import java.io.File
 
 object Euler89 {
+  private def romanToDecimal(char: Char) = char match {
+    case 'I' => 1
+    case 'V' => 5
+    case 'X' => 10
+    case 'L' => 50
+    case 'C' => 100
+    case 'D' => 500
+    case 'M' => 1000
+  }
+
+  private def highestRoman(value: Int) = {
+    validRomans.find { suggestions =>
+      suggestions._2 <= value
+    }.get
+  }
+
+  private def romanStringToInteger(str: String) = {
+    var current = Option.empty[Char]
+    var next    = Option.empty[Char]
+
+    def currentValue = romanToDecimal(current.get)
+
+    def nextValue = romanToDecimal(next.get)
+
+    var sum = 0
+    str.foreach { char =>
+      if (current.isEmpty) {
+        current = Some(char)
+      } else {
+        next = Some(char)
+        if (currentValue >= nextValue) {
+          sum += currentValue
+        } else {
+          sum -= currentValue
+        }
+        current = next
+      }
+    }
+    sum += currentValue
+    sum
+  }
+
+  private val validRomans = List(
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I"
+  ).map { e =>
+    (e, romanStringToInteger(e))
+  }
+
+  private def integerToRoman(value: Int) = {
+    var todo   = value
+    var result = new StringBuilder()
+    while (todo > 0) {
+      val (roman, singleValue) = highestRoman(todo)
+      todo -= singleValue
+      result append roman
+    }
+    result.toString
+  }
+
   def main(args: Array[String]): Unit = {
     measured {
       solveIt
@@ -12,76 +83,6 @@ object Euler89 {
   private def solveIt = {
     val numerals   = new File("resource/roman.txt").slurp.toList
     val totalChars = numerals.mkString.length
-
-    def romanToDecimal(char: Char) = char match {
-      case 'I' => 1
-      case 'V' => 5
-      case 'X' => 10
-      case 'L' => 50
-      case 'C' => 100
-      case 'D' => 500
-      case 'M' => 1000
-    }
-
-    val validRomans = List(
-      "M",
-      "CM",
-      "D",
-      "CD",
-      "C",
-      "XC",
-      "L",
-      "XL",
-      "X",
-      "IX",
-      "V",
-      "IV",
-      "I"
-    )
-
-    def highestRoman(value: Int) = {
-      validRomans.find { suggestions =>
-        romanStringToInteger(suggestions) <= value
-      }.get
-    }
-
-    def romanStringToInteger(str: String) = {
-      var current = Option.empty[Char]
-      var next    = Option.empty[Char]
-
-      def currentValue = romanToDecimal(current.get)
-
-      def nextValue = romanToDecimal(next.get)
-
-      var sum = 0
-      str.foreach { char =>
-        if (current.isEmpty) {
-          current = Some(char)
-        } else {
-          next = Some(char)
-          if (currentValue >= nextValue) {
-            sum += currentValue
-          } else {
-            sum -= currentValue
-          }
-          current = next
-        }
-      }
-      sum += currentValue
-      sum
-    }
-
-    def integerToRoman(value: Int) = {
-      var todo   = value
-      var result = ""
-      while (todo > 0) {
-        val roman = highestRoman(todo)
-        val next  = romanStringToInteger(roman)
-        todo -= next
-        result += roman
-      }
-      result
-    }
 
     def optimize(roman: String) = integerToRoman(romanStringToInteger(roman))
 
