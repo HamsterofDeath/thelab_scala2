a66c19873c1c13a208be00c956ed90770517f4d6
HamsterofDeath <h-star@gmx.de>
2020-04-26 00:14:52 +0200

    euler 26

--- src/main/scala/hod/euler/Euler26.scala
+++ src/main/scala/hod/euler/Euler26.scala
@@ -4,36 +4,36 @@ import scala.collection.mutable
 
 object Euler26 {
   def main(args: Array[String]): Unit = {
-    def postZeroDigits(n: Int) = {
-      var left = 1
-      val right = n
-      val digits = mutable.ArrayBuffer.empty[Int]
-      val knownRemainders = mutable.HashSet.empty[Int]
-      val remaindersInOrder = mutable.ArrayBuffer.empty[Int]
-      def done = left == 0 || knownRemainders(left)
-      while (!done) {
-        knownRemainders += left
-        remaindersInOrder += left
-        while (left < right) {
-          left *= 10
+    measured {
+      def postZeroDigits(n: Int) = {
+        var left = 1
+        val right = n
+        val digits = mutable.ArrayBuffer.empty[Int]
+        val remaindersInOrder = mutable.ArrayBuffer.empty[Int]
+        def done = left == 0 || remaindersInOrder.contains(left)
+        while (!done) {
+          remaindersInOrder += left
+          while (left < right) {
+            left *= 10
+          }
+          digits += left / right
+          left = left % right
         }
-        digits += left / right
-        left = left % right
-      }
-      if (knownRemainders(left)) {
-        val recurringCycleMarker = left
-        val from = {
-          remaindersInOrder.indexOf(recurringCycleMarker)
-        }
+        if (remaindersInOrder.contains(left)) {
+          val recurringCycleMarker = left
+          val from = {
+            remaindersInOrder.indexOf(recurringCycleMarker)
+          }
 
-        (digits.take(from), digits.slice(from, digits.size))
-      } else {
-        (digits, Nil)
+          (digits.take(from), digits.slice(from, digits.size))
+        } else {
+          (digits, Nil)
+        }
       }
-    }
-    1 to 999 foreach { i =>
-      val (digits, cycle) = postZeroDigits(i)
-      println(s"$i -> 0.${digits.mkString}(${cycle.mkString})")
+      val solution = (1 to 999).iterator.maxBy { i =>
+        postZeroDigits(i)._2.size
+      }
+      println(solution)
     }
   }
 }
