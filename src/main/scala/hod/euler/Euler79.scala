0073f6a5ed99ec83bfa64dfae856b35d0ffb8e47
HamsterofDeath <h-star@gmx.de>
2020-05-03 01:19:50 +0200

    euler 79

---
+++ src/main/scala/hod/euler/Euler79.scala
@@ -0,0 +1,26 @@
+package hod.euler
+
+import java.io.File
+
+object Euler79 {
+  def main(args: Array[String]): Unit = {
+    val in = new File("resource/keylog.txt").slurp.toList
+    def testNumber(str: String) = {
+      in.forall { pin =>
+        val indexes = pin.map(c => str.indexOf(c))
+        indexes.forall(_ >= 0) &&
+        indexes(0) < indexes(1) &&
+        indexes(1) < indexes(2)
+      }
+    }
+    def solutions = Iterator.from(3).flatMap { length =>
+      val tens = Iterator.continually(10)
+      val start = tens.take(length - 1).product
+      val end = start * 10 - 1
+      (start to end).iterator.map(_.toString).find(testNumber)
+    }
+    measured {
+      println(solutions.next())
+    }
+  }
+}
