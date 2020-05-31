ec8c068033fbe8bb4dbbfb7e9e09fdc8b7afae42
HamsterofDeath <h-star@gmx.de>
2020-05-31 15:50:30 +0200

    ...

--- src/main/scala/hod/training/sscala/ScalaSnake.scala
+++ src/main/scala/hod/training/sscala/ScalaSnake.scala
@@ -5,11 +5,17 @@ import java.awt.{Color, Graphics}
 import hod.training.EmptyFrame
 
 object ScalaSnake {
+
+  var  spielerx= 0
+var spielery= 0
   class Spiel extends EmptyFrame {
     override def zeichnen(g:Graphics): Unit = {
-      g.setColor(Color.green)
-      g.fillRoundRect(0,0,100,100,75,75)
-    }
+g.setColor(Color.GREEN)
+  g.fillRoundRect(spielerx,spielery,100, 100,80,80)  }
+    if (oben) spielery = spielery-1 else
+    if (unten) spielery= spielery+1 else
+    if (links) spielerx= spielerx-1 else
+   if  ( rechts)spielerx = spielerx +1
   }
 
   def main(args: Array[String]): Unit = {
