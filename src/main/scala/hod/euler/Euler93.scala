49b688d3f6a5091b68b906e0093b0b28bd5e6f9c
HamsterofDeath <h-star@gmx.de>
2020-06-08 18:23:14 +0200

    euler 93

--- src/main/scala/hod/euler/Euler93.scala
+++ src/main/scala/hod/euler/Euler93.scala
@@ -1,5 +1,7 @@
 package hod.euler
 
+import scala.util.Try
+
 object Euler93 {
   sealed trait Calculation {
     def calculate(a: BigDecimal, b: BigDecimal): BigDecimal
@@ -10,35 +12,40 @@ object Euler93 {
   case object Sub extends Calculation {
     override def calculate(a: BigDecimal, b: BigDecimal): BigDecimal = a - b
   }
+  case object SubFlip extends Calculation {
+    override def calculate(a: BigDecimal, b: BigDecimal): BigDecimal = b - a
+  }
   case object Mul extends Calculation {
     override def calculate(a: BigDecimal, b: BigDecimal): BigDecimal = a * b
+  }
+  case object DivFlip extends Calculation {
+    override def calculate(a: BigDecimal, b: BigDecimal): BigDecimal = b / a
   }
   case object Div extends Calculation {
     override def calculate(a: BigDecimal, b: BigDecimal): BigDecimal = a / b
   }
 
   def main(args: Array[String]): Unit = {
-    val calculations =
-      List(Add, Sub, Mul, Div).flatMap(e => List(e, e, e))
-    val comb =
-      calculations.combinations(3).toList.flatMap(_.permutations).distinct
+    val solution = measured {
+      val calculations =
+        List(Add, Sub, Mul, Div, DivFlip, SubFlip).flatMap(e => List(e, e, e))
+      val comb =
+        calculations.combinations(3).toList.flatMap(_.permutations).distinct
 
-    def findAll(digits: List[Int]) = {
-      val reachable = digits.permutations
-        .flatMap { digitPerm =>
-          comb.flatMap { calc =>
-            calculateOne(digitPerm, calc)
+      def findAll(digits: List[Int]) = {
+        val reachable = digits.permutations
+          .flatMap { digitPerm =>
+            comb.flatMap { calc =>
+              calculateConfiguration(digitPerm, calc)
+            }
           }
-        }
-        .toSet
-        .toList
-        .sorted
-      reachable
-    }
+          .toSet
+          .toList
+          .sorted
+        reachable
+      }
 
-    // val pool = (1 to 9).combinations(4).map(_.toList).toList
-    def pool = List(List(2, 3, 8, 9))
-    def solution =
+      val pool = (1 to 9).combinations(4).map(_.toList).toList
       pool
         .map { subSet =>
           val chainUntil = {
@@ -55,16 +62,14 @@ object Euler93 {
           (subSet, chainUntil)
         }
         .maxBy(_._2)
-
+    }
     println(s"Solution: $solution")
-
-    //  calculateOne(List(2, 3, 8, 9), List(Mul, Sub, Mul))
   }
 
-  private def calculateOne(digits: List[Int], calc: List[Calculation]) = {
+  private def calculateConfiguration(digits: List[Int],
+                                     calc: List[Calculation]) = {
     val concreteResults = {
-      val a = {
-        //(((a # b) # c) # d)
+      Try {
         val first = calc(0)
           .calculate(digits(0), digits(1))
         val second = calc(1)
@@ -72,39 +77,11 @@ object Euler93 {
         val third = calc(2)
           .calculate(second, digits(3))
         third
-      }
-
-      val b = {
-        //(a # b) # (c # d)
-        val first = calc(0)
-          .calculate(digits(0), digits(1))
-        val second = calc(2)
-          .calculate(digits(2), digits(3))
-        val third = calc(1)
-          .calculate(first, second)
-        third
-      }
-
-      val c = {
-        //a # (b # c # d)
-        val first = calc(0)
-          .calculate(digits(0), digits(1))
-        val second = calc(1)
-          .calculate(first, digits(2))
-        val third = calc(2)
-          .calculate(second, digits(3))
-        third
-      }
-
-      List(a, b, c)
+      }.toOption
     }
-    concreteResults.filter(_.isWhole).map(_.toInt).filter(_ > 0).map { e =>
-      println(
-        s"(((${digits(0)} ${calc(0)} ${digits(1)}) ${calc(1)} ${digits(2)}) ${calc(
-          2
-        )} ${digits(3)}) = $e"
-      )
-      e
-    }
+    concreteResults
+      .filter(_.isWhole)
+      .map(_.toInt)
+      .filter(_ > 0)
   }
 }
