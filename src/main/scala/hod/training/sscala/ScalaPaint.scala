1967ad8bbd2d9df79f7d549c3a7e38747eb93d8f
Dennis Haupt <d.haupt@veact.net>
2020-06-09 21:48:15 +0200

    ...

--- src/main/scala/hod/training/sscala/ScalaPaint.scala
+++ src/main/scala/hod/training/sscala/ScalaPaint.scala
@@ -1,13 +1,16 @@
 package hod.training.sscala
 
 import java.awt.Graphics
+import java.awt.image.BufferedImage
 
 import hod.training.EmptyFrame
 
 object ScalaPaint {
 
   class Spiel extends EmptyFrame {
+    val bild = new BufferedImage(50,50, BufferedImage.TYPE_INT_ARGB)
     override def zeichnen(g: Graphics): Unit = {
+
     }
   }
 
