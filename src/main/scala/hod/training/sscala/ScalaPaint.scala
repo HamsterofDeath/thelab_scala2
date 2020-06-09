263d24fde80edf1c121f974d10e583dab5509af8
Dennis Haupt <d.haupt@veact.net>
2020-06-09 21:52:00 +0200

    ...

--- src/main/scala/hod/training/sscala/ScalaPaint.scala
+++ src/main/scala/hod/training/sscala/ScalaPaint.scala
@@ -1,7 +1,7 @@
 package hod.training.sscala
 
-import java.awt.Graphics
 import java.awt.image.BufferedImage
+import java.awt.{Color, Graphics}
 
 import hod.training.EmptyFrame
 
@@ -10,6 +10,17 @@ object ScalaPaint {
   class Spiel extends EmptyFrame {
     val bild = new BufferedImage(50,50, BufferedImage.TYPE_INT_ARGB)
     override def zeichnen(g: Graphics): Unit = {
+      1 to 10 foreach { x =>
+        1 to 10 foreach { y =>
+          bild.setRGB(10+x,10+y, new Color(0,0,255).getRGB)
+        }
+      }
+
+      val g2 = bild.getGraphics
+      g2.setColor(Color.green)
+      g2.fillRect(5,5,5,5)
+      g.drawImage(bild,25,25,50,50, null)
+
 
     }
   }
