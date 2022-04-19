aa433fe6d5ffaa6fb159749fcb157121fbe2f4fc
HamsterofDeath <h-star@gmx.de>
2022-04-19 15:53:02 +0200

    ...

--- src/main/scala/hod/javacatchup/Euler71.java
+++ src/main/scala/hod/javacatchup/Euler71.java
@@ -2,11 +2,17 @@ package hod.javacatchup;
 
 import hod.EulerUtils;
 
+import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.IntStream;
 
 public class Euler71 {
+    record Tuple(int n, int d){
+        double asDouble() {
+            return (double) n/d;
+        }
+    }
     public static void main(String[] args) {
         var reference = 3.0 / 7;
         var limit = 1000000;
@@ -14,19 +20,23 @@ public class Euler71 {
         var result = IntStream.range(1, limit)
                 .boxed()
                 .parallel()
-                .flatMap(d -> {
+                .map(d -> {
                     if(closest.incrementAndGet()%1000==0) {
                         System.out.print(".");
                     }
 
-                    return IntStream.range(1, d - 1)
+                    var smallestNumerator =  IntStream.range(1, d - 1)
                             .takeWhile(n -> (double) n / d < reference)
                             .filter(n->EulerUtils.isReducedProperFractionJ(n,d))
-                            .mapToDouble(n -> reference - (double) n / d)
-                            .min()
-                            .stream()
-                            .boxed();
-                }).min(Double::compareTo);
+                            .boxed()
+                            .min((o1, o2) -> {
+                                var fractionLeft = reference - o1 / d;
+                                var fractionRight = reference - o2 / d;
+                                return Double.compare(fractionLeft, fractionRight);
+                            })
+                            .get();
+                    return new Tuple(smallestNumerator ,d);
+                }).min(Comparator.comparingDouble(o -> reference - o.asDouble()));
         System.out.println(result);
 
     }
