4b4dcde5a036387272c0c13cc61d8c14a1b21129
hamsterofdeath <d.haupt82@gmail.com>
2022-04-19 17:14:41 +0200

    ....

--- src/main/scala/hod/javacatchup/Euler71.java
+++ src/main/scala/hod/javacatchup/Euler71.java
@@ -5,14 +5,16 @@ import hod.EulerUtils;
 import java.util.Comparator;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 public class Euler71 {
-    record Tuple(int n, int d){
+    record Tuple(int n, int d) {
         double asDouble() {
-            return (double) n/d;
+            return (double) n / d;
         }
     }
+
     public static void main(String[] args) {
         var reference = 3.0 / 7;
         var limit = 1000000;
@@ -21,22 +23,23 @@ public class Euler71 {
                 .boxed()
                 .parallel()
                 .map(d -> {
-                    if(closest.incrementAndGet()%1000==0) {
+                    if (closest.incrementAndGet() % 1000 == 0) {
                         System.out.print(".");
                     }
 
-                    var smallestNumerator =  IntStream.range(1, d - 1)
+                    var smallestNumerator = IntStream.range(1, d - 1)
                             .takeWhile(n -> (double) n / d < reference)
-                            .filter(n->EulerUtils.isReducedProperFractionJ(n,d))
+                            .filter(n -> EulerUtils.isReducedProperFractionJ(n, d))
                             .boxed()
                             .min((o1, o2) -> {
-                                var fractionLeft = reference - o1 / d;
-                                var fractionRight = reference - o2 / d;
+                                var fractionLeft = reference - (double) o1 / d;
+                                var fractionRight = reference - (double) o2 / d;
                                 return Double.compare(fractionLeft, fractionRight);
                             })
-                            .get();
-                    return new Tuple(smallestNumerator ,d);
-                }).min(Comparator.comparingDouble(o -> reference - o.asDouble()));
+                            .orElseGet(() -> d - 1);
+                 //   System.out.println("Candidate: "+smallestNumerator + "/"+d+"="+(smallestNumerator/(double)d));
+                    return new Tuple(smallestNumerator, d);
+                }).min(Comparator.comparingDouble(o -> Math.abs(reference - o.asDouble())));
         System.out.println(result);
 
     }
